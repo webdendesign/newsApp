9d1e5a3fa3e4dc2194db3abe0bf43d9f813011d3
Den <fabri.din@yandex.ru>
2019-11-18 20:04:17 +0500

    work server

--- app.js
+++ app.js
@@ -73,6 +73,16 @@ const newsService = (function() {
   };
 })();
 
+// Elements
+const form = document.forms["newsControls"];
+const countrySelect = form.elements["country"];
+const searchInput = form.elements["search"];
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  loadNews();
+});
+
 //  init selects
 document.addEventListener("DOMContentLoaded", function() {
   M.AutoInit();
@@ -81,17 +91,40 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // load news function
 function loadNews() {
-  newsService.topHeadlines("ua", onGetResponse);
+  showLoader();
+  const country = countrySelect.value;
+  const searchText = searchInput.value;
+
+  if (!searchText) {
+    newsService.topHeadlines(country, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
 }
 
 //function on get response from server
 function onGetResponse(err, res) {
+  removePreLoader();
+  if (err) {
+    showAlert(err, "error-msg");
+    return;
+  }
+
+  if (!res.articles.length) {
+    //show empty message
+    return;
+  }
   renderNews(res.articles);
 }
 
 //function render news
 function renderNews(news) {
   const newsContainer = document.querySelector(".news-container .row");
+
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+
   let fragment = "";
 
   news.forEach(newsItem => {
@@ -100,6 +133,16 @@ function renderNews(news) {
   });
 
   newsContainer.insertAdjacentHTML("afterbegin", fragment);
+}
+
+//function clear container
+function clearContainer(container) {
+  //container.innerHTML = '';
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
 }
 
 // News item template function
@@ -120,4 +163,28 @@ function newsTemplate({ urlToImage, title, url, description }) {
       </div>
     </div>
   `;
+}
+
+function showAlert(msg, type = "success") {
+  M.toast({ html: msg, classes: type });
+}
+
+//show loader function
+
+function showLoader() {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="progress">
+      <div class="indeterminate"></div>
+    </div>
+    `
+  );
+}
+
+// remove loader function
+function removePreLoader() {
+  const loader = document.querySelector(".progress");
+  if (loader) {
+    loader.remove();
+  }
 }
